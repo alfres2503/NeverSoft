3457cf287662219b2c692f5d562aa6c4c74f613b
alfres2503 <lusuarezag@est.utn.ac.cr>
2023-02-21 13:15:06 -0600

    Tablas con filtrado y rubros, listo avance 3

---
+++ Web/Scripts/Tables.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#myTable').DataTable({
+        "paging": false
+    });
+});
+
+$(document).ready(function () {
+    $('#DebtsTable').DataTable({
+        "paging": false
+    });
+});
